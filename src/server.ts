b4abb5a71a877e77ab3bd1ed45d8bf2bf71becab
Abhinav Tushar <abhinav@lepisma.xyz>
2020-05-02 15:52:06 +0530

    remove old comments

--- src/server.ts
+++ src/server.ts
@@ -18,19 +18,13 @@ import {
 import { parse } from './parsers';
 import { complete } from './completors';
 
-// Create a connection for the server. The connection uses Node's IPC as a transport.
-// Also include all preview / proposed LSP features.
 let connection = createConnection(ProposedFeatures.all);
-
-// Create a simple text document manager. The text document manager
-// supports full document sync only
 let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 connection.onInitialize((_params: InitializeParams) => {
   return {
     capabilities: {
       textDocumentSync: TextDocumentSyncKind.Full,
-      // Tell the client that the server supports code completion
       completionProvider: {
         resolveProvider: true
       }
@@ -38,13 +32,8 @@ connection.onInitialize((_params: InitializeParams) => {
   };
 });
 
-// This handler provides the initial list of the completion items.
 connection.onCompletion(
   (textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-    // The pass parameter contains the position of the text document in
-    // which code complete got requested. For the example we ignore this
-    // info and always provide the same completion items.
-
     let doc = documents.get(textDocumentPosition.textDocument.uri);
     if (doc) {
       let buffer = parse(doc, textDocumentPosition.position);
@@ -64,9 +53,6 @@ connection.onCompletion(
   }
 );
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
 documents.listen(connection);
 
-// Listen on the connection
 connection.listen();
